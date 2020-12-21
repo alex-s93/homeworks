e900f68db05257ca840a5cd39d6d95376b7fb89b
Alex Solodkov <solodkov.alex@gmail.com>
2020-12-21 15:16:00 +0200

    Fix for "No comments in such simple code" issue

--- com/alevel/homework3/ZeroInsteadOfEven.java
+++ com/alevel/homework3/ZeroInsteadOfEven.java
@@ -2,10 +2,8 @@ package com.alevel.homework3;
 
 public class ZeroInsteadOfEven {
     public static void main(String[] args) {
-        // Create array
         int[] array = new int[2000];
 
-        // fill array with numbers
         for (int i = 0; i < 2000; i++) {
             array[i] = (int) (Math.random() * 100 + 1); // (+ 1) for no 0 in the array
         }
--- com/alevel/homework3/task2/GeometricAverage.java
+++ com/alevel/homework3/task2/GeometricAverage.java
@@ -2,15 +2,12 @@ package com.alevel.homework3.task2;
 
 public class GeometricAverage {
     public static void main(String[] args) {
-        // Create array
         int[] array = new int[400];
 
-        // fill array with numbers
         for (int i = 0; i < 400; i++) {
             array[i] = (int) (Math.random() * 10 + 1); // (+ 1) for no 0 in the array
         }
         
-        // Get the result of multiplying elements
         double multiplyingResult = 1;
         double rate = 0.0025;               // 1/400 = 0.0025
         for (int i = 0; i < 400; i++) {
