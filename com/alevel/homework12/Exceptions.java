4b79b9da6a5eb3bb9a5a591271d25f699b4cde54
Alex Solodkov <solodkov.alex@gmail.com>
2021-01-25 16:35:58 +0200

    HW12. Refactored Exceptions class. Finish HW

--- com/alevel/homework12/Exceptions.java
+++ com/alevel/homework12/Exceptions.java
@@ -2,30 +2,42 @@ package com.alevel.homework12;
 
 public class Exceptions {
     public static void main(String[] args) {
-//        ClassCastExceptionTest();
-//        NullPointerExceptionTest();
-//        NumberFormatExceptionTest();
-        ArrayIndexOutOfBoundsExceptionTest();
+        ClassCastExceptionTest("test");
+        NullPointerExceptionTest(null);
+        NumberFormatExceptionTest("test1");
+        ArrayIndexOutOfBoundsExceptionTest(new String[]{"test1", "test2"}, 2);
     }
 
-    static void ClassCastExceptionTest() {
-        Object x = "test";
-        System.out.println((int) x);
+    static void ClassCastExceptionTest(Object x) {
+        try {
+            System.out.println((int) x);
+        } catch (ClassCastException e) {
+            System.out.println("ClassCastException message: " + e.getMessage());
+        }
     }
 
-    static void NullPointerExceptionTest() {
-        Integer x = null;
-        System.out.println(x * 2);
+    static void NullPointerExceptionTest(Integer x) {
+        try {
+            System.out.println(x * 2);
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException message: " + e.getMessage());
+        }
     }
 
-    static void NumberFormatExceptionTest() {
-        String[] a = {"test1", "test2"};
-        Integer.parseInt(a[0]);
+    static void NumberFormatExceptionTest(String value) {
+        try {
+            Integer.parseInt(value);
+        } catch(NumberFormatException e) {
+            System.out.println("NumberFormatException message: " + e.getMessage());
+        }
     }
 
-    static void ArrayIndexOutOfBoundsExceptionTest() {
-        String[] a = {"test1", "test2"};
-        System.out.println(a[2]);
+    static void ArrayIndexOutOfBoundsExceptionTest(String[] strings, int index) {
+        try {
+            System.out.println(strings[index]);
+        } catch(ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBoundsException message: " + e.getMessage());
+        }
     }
 
     /**
