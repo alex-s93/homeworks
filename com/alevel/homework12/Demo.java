79e6de892eb1491902c71d536fd154706946a2ed
Alex Solodkov <solodkov.alex@gmail.com>
2021-01-25 14:08:22 +0200

    HW12. Refactored Demo class with try-with-res

--- com/alevel/homework12/Demo.java
+++ com/alevel/homework12/Demo.java
@@ -1,30 +1,28 @@
 package com.alevel.homework12;
 
 import java.io.*;
-import java.util.Scanner;
 
 public class Demo {
-    public static String read(String path) throws IOException {
-//открываем файл по пути
-        BufferedReader reader = new BufferedReader(new FileReader(path));
+    public static String read(String path) {
         StringBuilder stringBuilder = new StringBuilder();
         String currentString;
-//построчно считываем файл
-        while ((currentString = reader.readLine()) != null) {
-            stringBuilder.append(currentString);
-            stringBuilder.append("\n");
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(path))) {
+            //построчно считываем файл
+            while ((currentString = reader.readLine()) != null) {
+                stringBuilder.append(currentString);
+                stringBuilder.append("\n");
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("FileNotFoundException message:" + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("IOException message:" + e.getMessage());
         }
-//закрываем файл
-        reader.close();
-//возвращаем вычитанный текст в строке
+        //возвращаем вычитанный текст в строке
         return stringBuilder.toString();
     }
 
     public static void main(String[] args) {
-        try{
-            System.out.println(read("/Users/non_exist_path/test.txt"));
-        } catch(IOException e) {
-            System.out.println("Message: " + e.getMessage());
-        }
+        System.out.println(read("/Users/qa/test.txt"));
     }
 }
