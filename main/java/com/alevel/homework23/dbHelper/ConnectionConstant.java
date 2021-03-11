715f742dafd2e4fbe405ee52ac88e206ffcc4e41
alex-s93 <57666472+alex-s93@users.noreply.github.com>
2021-03-11 16:47:42 +0200

    Update ConnectionConstant.java

--- main/java/com/alevel/homework23/dbHelper/ConnectionConstant.java
+++ main/java/com/alevel/homework23/dbHelper/ConnectionConstant.java
@@ -3,6 +3,6 @@ package com.alevel.homework23.dbHelper;
 public class ConnectionConstant {
     public static final String URL = "jdbc:mysql://localhost:3306/db_shop";
     public static final String USER = "root";
-    public static final String PASSWORD = "Great123";
+    public static final String PASSWORD = "******";
 }
 
